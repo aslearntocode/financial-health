edd80db225783b152d2f58470450196a513d4108
aslearntocode <aslearntocode@gmail.com>
2025-01-15 22:50:42 +0530

    fixed the API call

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
-  // Disable the warning about the .next directory
-  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig 
